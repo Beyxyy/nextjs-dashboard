415a67810d3fbac48a469c271accb98181b9a0bc
Anthony KALBE <anthony.kalbe@cibeo.fr>
2024-07-02 17:29:24 +0200

    chore: Update language to French in formatDateToLocal function

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -9,7 +9,7 @@ export const formatCurrency = (amount: number) => {
 
 export const formatDateToLocal = (
   dateStr: string,
-  locale: string = 'en-US',
+  locale: string = 'fr-FR',
 ) => {
   const date = new Date(dateStr);
   const options: Intl.DateTimeFormatOptions = {
